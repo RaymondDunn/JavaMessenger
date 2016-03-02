c4cbe79c838ba0891f2907ae61a78065f5793507
Raymond Dunn <ray.l.dunn@gmail.com>
2016-03-02 13:44:05 -0500

    small edits to show chris

--- src/myMessenges/Message.java
+++ src/myMessenges/Message.java
@@ -1,5 +1,28 @@
 package myMessenges;
 
-public abstract class Message {
+import java.nio.charset.Charset;
 
+/** Abstract base class for messages exchanged with myMessenger
+ * @author Ray and Chris
+ *
+ */
+public abstract class Message {
+	
+	
+	enum messageType { TEXTMESSAGE, FILEMESSAGE, TERMINATIONMESSAGE };
+//	final static byte TEXTMESSAGE = 0xTEXTMESSAGE;
+//	final static byte FILEMESSAGE = 0xFILEMESSAGE;
+//	final static byte TERMINATIONMESSAGE = 0xTERMINATIONMESSAGE;
+//	byte[] 
+	
+	Charset characterSet = Charset.defaultCharset();
+	
+	/** Default constructor for message to be exchanged between
+	 * client and host. 
+	 * 
+	 */
+	Message(byte messageType){
+		
+	}
+	
 }
