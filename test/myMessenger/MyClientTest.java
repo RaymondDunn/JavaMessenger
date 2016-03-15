f841b22679435ea46e0112fe651fc9890a0c6995
Raymond Dunn <ray.l.dunn@gmail.com>
2016-03-14 20:29:22 -0400

    more tests...

---
+++ test/myMessenger/MyClientTest.java
@@ -0,0 +1,14 @@
+package myMessenger;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyClientTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
